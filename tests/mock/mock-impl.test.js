9421e7b2be7fefeb9ae1b36447745442bed7276b
mtnori <moto44.noriyuki@gmail.com>
2018-07-15 09:13:16 +0900

    mockImplementationOnceの処理を追加

--- tests/mock/mock-impl.test.js
+++ tests/mock/mock-impl.test.js
@@ -9,5 +9,41 @@ test('mock implementation', () => {
   myMockFn((err, val) => console.log(val));
 
   foo.mockImplementation(() => 42);
-  console.log(foo());
+  console.log(foo()); // > 42
+
+  // 複数回の関数呼び出しで異なる結果を生成する必要がある場合、
+  // mockImplementationOnceメソッドを使用する
+  const myMockFn2 = jest
+    .fn()
+    .mockImplementationOnce(cb => cb(null, true))
+    .mockImplementationOnce(cb => cb(null, false));
+  myMockFn2((err, val) => console.log(val)); // > true
+  myMockFn2((err, val) => console.log(val)); // > false
+
+  // mockImplementationOnceで定義された実装を使い果たした場合、
+  // jest,fnで定義した実装を使います(定義されている場合)
+  const myMockFn3 = jest
+    .fn(() => 'default')
+    .mockImplementationOnce(() => 'first call')
+    .mockImplementationOnce(() => 'second call');
+  console.log(myMockFn3(), myMockFn3(), myMockFn3(), myMockFn3());
+  // > 'first call', 'second call', 'default', 'default'
+
+  // thisを返す必要がある場合、糖衣構文が存在する
+  const myObj = {
+    myMethod: jest.fn().mockReturnThis()
+  };
+  // is the same
+  const otherObj = {
+    myMethod: jest.fn(function() {
+      return this;
+    })
+  };
+
+  // モック関数に名前を与えることができる
+  const myMockFn4 = jest
+    .fn()
+    .mockReturnValue('default')
+    .mockImplementation(scalar => 42 + scalar)
+    .mockName('add42');
 });
